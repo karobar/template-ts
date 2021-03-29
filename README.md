2c5c553169b00475f134e5f91ba8961cf89e2125
Travis P <travis.pressler@scientiallc.com>
2021-03-29 15:22:49 -0400

    Husky + ESLint + EditorConfig integration

--- README.md
+++ README.md
@@ -1,7 +1,22 @@
-# To initialize template
+# Electron Template
+
+Clone this repo.
+
+This template will run linting tools before each `git push`.
+
+#### Components
+* [ESLint (linting)](https://eslint.org/docs/user-guide/getting-started)
+* [husky (git hook management)](https://typicode.github.io/husky/#/)
+* [EditorConfig (IDE configuration)](https://editorconfig.org/) 
+
+# Initialize Template
 
     ./prepare.sh
 
 # To run
 
-    npm start
+    yarn start
+
+# To test
+
+    yarn test
--- src/main.js
+++ src/main.js
@@ -18,29 +18,28 @@ const { app, BrowserWindow } = require('electron')
 const path = require('path')
 
 function createWindow () {
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            preload: path.join(__dirname, 'preload.js')
-        }
-    })
-
-    win.loadFile('public/index.html').then(_ => { })
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+  win.loadFile('public/index.html').then(_ => { })
 }
 
 app.whenReady().then(() => {
-    createWindow()
+  createWindow()
 
-    app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) {
-            createWindow()
-        }
-    })
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
 })
 
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit()
-    }
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
 })
